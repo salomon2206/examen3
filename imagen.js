ee0c3ea62bec8800f89c059f8725fe2f74040037
salomon <crisanto.jimenez_123@hotmail.com>
2021-11-30 21:58:11 -0600

    mi primer commit

---
+++ imagen.js
@@ -0,0 +1,20 @@
+function Cambiar(){
+  document.getElementById("img1").src = "imagenes/indice.jpg";
+
+}
+function iniciar(){
+  document.getElementById("img1").src= "imagenes/disney.jpg";
+
+}
+function iniciar1(){
+  document.getElementById("img1").src="imagenes/bad.jpg";
+}
+function cambiar(){
+  document.getElementById("img1").src="imagenes/zombie.jpg"
+}
+
+function restaurar(){
+  document.getElementById("img1").src = "imagenes/Toy_Story.jpg";
+
+
+}
